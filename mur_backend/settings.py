eafa17a0c5e2ea7839dcf081618332b4a40370db
Kingsleu <ebenndombi99@gmail.com>
2025-11-19 11:37:31 +0100

    depoy database 11

--- mur_backend/settings.py
+++ mur_backend/settings.py
@@ -87,11 +87,11 @@ WSGI_APPLICATION = 'mur_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT', '3306'),
+        'NAME': os.getenv('MYSQLDATABASE','u911414181_citecmp'),
+        'USER': os.getenv('MYSQLUSER','u911414181_sitecmp'),
+        'PASSWORD': os.getenv('MYSQLPASSWORD','Jesusmas1234@'),
+        'HOST': os.getenv('MYSQLHOST','temoignage.eglisecmp.com'),
+        'PORT': os.getenv('MYSQLPORT', '3306'),
         'OPTIONS': {
             'ssl': {'ssl_disabled': True},
             'charset': 'utf8mb4',
