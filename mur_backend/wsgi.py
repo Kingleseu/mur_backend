ecfe2b95aa0c2863d32e42957ba4c62b462212b3
Kingsleu <ebenndombi99@gmail.com>
2025-11-19 03:05:24 +0100

    depoy database 6

--- mur_backend/wsgi.py
+++ mur_backend/wsgi.py
@@ -11,6 +11,16 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mur_backend.settings')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mur_backend.settings')
 
-application = get_wsgi_application()
+#application = get_wsgi_application()
+
+
+def simple_app(environ, start_response):
+    status = '200 OK'
+    output = b'App loads correctly'
+    response_headers = [('Content-type', 'text/plain'), ('Content-Length', str(len(output)))]
+    start_response(status, response_headers)
+    return [output]
+
+application = simple_app
